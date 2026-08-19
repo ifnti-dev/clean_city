17de399d513f394be9371dd28087cabde97984df
ganietou <ganietoukondi8@gmail.com>
2026-08-19 16:09:46 +0000

    creation des seeders

---
+++ database/seeders/MenageSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MenageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
