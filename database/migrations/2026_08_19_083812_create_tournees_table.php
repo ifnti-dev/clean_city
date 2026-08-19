1ff2b6c81818a24ca72e6339816dac2c8f1905ac
ganietou <ganietoukondi8@gmail.com>
2026-08-19 18:05:54 +0000

    correction migration

--- database/migrations/2026_08_19_083812_create_tournees_table.php
+++ database/migrations/2026_08_19_083812_create_tournees_table.php
@@ -16,6 +16,19 @@ return new class extends Migration
             $table->enum('statut', ['PREVU', 'EN_COUR', 'TERMINEE']);
             $table->date('date');
             $table->string('itineraire');
+            $table->unsignedInteger('employe_id');
+            $table->unsignedInteger('zone_id');
+
+            $table->foreign('employe_id')
+                ->references('id')
+                ->on('employes')
+                ->onDelete('set null');
+
+            $table->foreign('zone_id')
+                ->references('id')
+                ->on('zones')
+                ->onDelete('set null');
+    
             $table->timestamps();
         });
     }
