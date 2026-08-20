2d51285168be51c46a7af3b001fd9e682a30251d
ganietou <ganietoukondi8@gmail.com>
2026-08-20 14:03:15 +0000

    message

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => ' focus:border-green-500  rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-green-300 focus:border-green-500  rounded-md shadow-sm']) }}>
