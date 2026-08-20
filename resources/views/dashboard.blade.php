13cd6cf6d1c948771fb16a1175f8ecdc39f2c263
ganietou <ganietoukondi8@gmail.com>
2026-08-20 09:16:19 +0000

    integration de template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.base')
+
+@section('content')
+    <div class="bg-grid-300"> Hello world </div>
+@endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,7 @@
     <meta name="description"
         content="Dash UI - TailwindCSS HTML Admin Template Free and open-source Github, provides developers with everything need to create Web Application & Kick start project" />
     <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon/favicon.ico" />
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Libs CSS -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -41,28 +41,37 @@
     <main>
         <!-- start the project -->
         <!-- app layout -->
-        <div id="app-layout" class="overflow-x-hidden flex">
+        <div id="app-layout" class="overflow-x-hidden flex"><!-- start navbar -->
+            @include('layouts.partials.sidebar')
+
 
             <div id="app-layout-content"
                 class="min-h-screen w-full min-w-[100vw] md:min-w-0 ml-[15.625rem] [transition:margin_0.25s_ease-out]">
                 <!-- start navbar -->
                 @include('layouts.partials.navbar')
 
+                <div class="h-16 pl-2 pr-2 pt-2">
+                    @yield('content')
+                </div>
+
+                    
+                    
+            
+
+                
             </div>
-            @include('layouts.partials.sidebar')
-
-
-        </div>
         </div>
         <!-- end of project -->
     </main>
 
+    <script src="./assets/libs/apexcharts/dist/apexcharts.min.js"></script>
     <script src="./assets/libs/feather-icons/dist/feather.min.js"></script>
     <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./assets/libs/simplebar/dist/simplebar.min.js"></script>
 
     <!-- Theme JS -->
     <script src="./assets/js/theme.min.js"></script>
+
 </body>
 
 </html>
