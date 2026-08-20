8495931799144f107b6bea305d884fc1c15ce82e
Sourouya-Abdoulaye <akisourouyaabdoulaye@gmail.com>
2026-08-20 13:57:41 +0000

    integration template

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => ' focus:border-green-500  rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' =>" border border-gray-300 text-gray-900
+                                          rounded focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2 px-3
+                                          disabled:opacity-50 disabled:pointer-events-none"]) }}>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,19 +1,42 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<html lang="en">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="description"
+        content="Dash UI - TailwindCSS HTML Admin Template Free and open-source Github, provides developers with everything need to create Web Application & Kick start project" />
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon/favicon.ico" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Libs CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" />
+    <link rel="stylesheet" href="./assets/libs/simplebar/dist/simplebar.min.css" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="./assets/css/theme.min.css">
+    <!-- Analytics Code -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8S4MT3EYG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-M8S4MT3EYG");
+    </script>
+
+    <link rel="stylesheet" href="./assets/libs/apexcharts/dist/apexcharts.css" />
+
+    <title>Dash UI - TailwindCSS HTML Admin Template Free</title>
+</head>
+
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
