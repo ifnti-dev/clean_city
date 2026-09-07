16610a8125400382f8a73f7cb508c821cbcca20a
ganietou <ganietoukondi8@gmail.com>
2026-09-07 09:55:26 +0000

    list article

--- resources/views/produits/liste_articles.blade.php
+++ resources/views/produits/liste_articles.blade.php
@@ -20,7 +20,7 @@
                 </a> --}}
 
                 <div class="flex-1">
-                    <a class="btn btn-ghost text-xl">Market </a>
+                   <h2>Market Place</h2>
                 </div>
 
                 <ul class="flex ml-auto items-center">
@@ -33,11 +33,10 @@
                                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
                             </svg>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-lg lg:left-auto lg:right-0"
-                            aria-labelledby="dropdownNotification">
+                        <div class="dropdown-menu dropdown-menu-lg lg:left-auto lg:right-0"aria-labelledby="dropdownNotification">
                             <div>
                                 <div class="border-b px-3 pt-2 pb-3 flex justify-between items-center">
-                                    <span class="text-lg text-gray-800 font-semibold">Notifications</span>
+                                    <span class="text-lg text-gray-800 font-semibold">panier</span>
                                     <a href="#">
                                         <span>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
